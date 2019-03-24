318f2ad6a29f5e43446fd8944f1586097e6d9326
Maksim <mimoz200385@gmail.com>
2019-03-24 16:32:27 +1000

    made simple template

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Container, Row, Col} from "react-bootstrap";
+import {Container, Row, Col, Button, InputGroup, FormControl, Nav} from "react-bootstrap";
 
 class App extends React.Component{
 
@@ -13,19 +13,38 @@ class App extends React.Component{
   render(){
     return(
       <Container style={{ height: '100%' }}>
-        <Row style={{height:'20%'}}>
+        <Row style={{height:'40%'}}>
           <Col>
-            <header>Header</header>
+            <Nav className="justify-content-end" activeKey="/home">
+              <Nav.Item>
+                <Nav.Link>Главная</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="link-1">Популярное</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="link-2">Войти</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="link-3"> Регистрация </Nav.Link>
+              </Nav.Item>
+            </Nav>
           </Col>
         </Row>
         <Row>
           <Col>
-            <main>Main</main>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <footer>Footer</footer>
+            <main>
+            <InputGroup className="mb-3">
+              <FormControl
+                  placeholder="Название проблемы"
+                  aria-label="Recipient's username"
+                  aria-describedby="basic-addon2"
+                />
+                <InputGroup.Append>
+                  <Button variant="outline-success">Поиск</Button>
+                </InputGroup.Append>
+              </InputGroup>
+            </main>
           </Col>
         </Row>
       </Container>
