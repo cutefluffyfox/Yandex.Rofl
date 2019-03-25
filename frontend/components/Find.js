255ed999818ec0b3c31511bf6840aec43a35966a
Maksim <mimoz200385@gmail.com>
2019-03-25 15:43:42 +1000

    send POST submit on server

--- frontend/components/Find.js
+++ frontend/components/Find.js
@@ -18,7 +18,34 @@ class Find extends React.Component{
 
   sendSubmit(){
     var letter = this.state.findString;
+    fetch('localhost:8000/Find',
+    {
+     method: 'post',
+     headers: {'Content-Type':'application/json'},
+     body: {
+      "searchValue": letter
+     }
+    })
+    .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+      // Examine the text in the response
+      response.json()
+      .then(function(data) {
+        console.log(data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
   }
+
+
 
 
 
