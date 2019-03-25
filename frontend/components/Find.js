c7518e0fdf977ce71c36d77662372a23afdf00f0
Maksim <mimoz200385@gmail.com>
2019-03-25 15:47:45 +1000

    add alertBox on empty SearchString

--- frontend/components/Find.js
+++ frontend/components/Find.js
@@ -18,31 +18,36 @@ class Find extends React.Component{
 
   sendSubmit(){
     var letter = this.state.findString;
-    fetch('localhost:8000/Find',
-    {
-     method: 'post',
-     headers: {'Content-Type':'application/json'},
-     body: {
-      "searchValue": letter
-     }
-    })
-    .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
-      // Examine the text in the response
-      response.json()
-      .then(function(data) {
-        console.log(data);
-      });
+    if(letter == ''){
+      alert("Пустая строка");
     }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
+    else{
+      fetch('localhost:8000/Find',
+      {
+       method: 'post',
+       headers: {'Content-Type':'application/json'},
+       body: {
+        "searchValue": letter
+       }
+      })
+      .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+        // Examine the text in the response
+        response.json()
+        .then(function(data) {
+          console.log(data);
+        });
+      }
+    )
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
+    });
+  }
   }
 
 
