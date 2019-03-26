9e46594006590af50b54e606795a069e25e82489
Baderik <baderikpavel@mail.ru>
2019-03-26 15:10:15 +1000

    Get POST on /Find and return;

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,5 @@
-from library.flask import Flask, render_template, url_for
-
+from library.flask import Flask, render_template, request
+from json import dumps
 
 app = Flask(__name__, template_folder='../frontend', static_folder='../frontend')
 
@@ -8,6 +8,17 @@ app = Flask(__name__, template_folder='../frontend', static_folder='../frontend'
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/Find', methods=['POST'])
+def find():
+    if request.method == 'POST':
+        data = eval(request.data.decode('utf-8'))
+
+        # Here ML is changing the data
+
+        return dumps(data)
+    return 'not post'
 
 
 if __name__ == '__main__':
